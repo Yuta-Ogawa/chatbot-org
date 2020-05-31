e3fcb86295e012ae71348539a29303750941739e
小川裕太 <oytkt0510@gmail.com>
2020-05-31 16:40:25 +0900

    chatbot-demoのコピー　コミット

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANZM40-3IvWDu4sExaoCGV1x5T8FG-YPg",
+    authDomain: "chatbot-demo-5fdc0.firebaseapp.com",
+    databaseURL: "https://chatbot-demo-5fdc0.firebaseio.com",
+    projectId: "chatbot-demo-5fdc0",
+    storageBucket: "chatbot-demo-5fdc0.appspot.com",
+    messagingSenderId: "554508662301",
+    appId: "1:554508662301:web:af429e2b517a0d66bd78f4",
+    measurementId: "G-H6HCSH0D33"
+  };
+
+  export default firebaseConfig
